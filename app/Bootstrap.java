7bc9bf36ccce339d5b01f37ed1a4a71caed03d6e
Linda Lynch <lindmlynch@gmail.com>
2022-05-29 19:07:21 +0100

    precise version of play

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -3,14 +3,17 @@ import java.util.List;
 import play.*;
 import play.jobs.*;
 import play.test.*;
- 
+
 import models.*;
- 
+
 @OnApplicationStart
-public class Bootstrap extends Job 
-{ 
+public class Bootstrap extends Job
+{
   public void doJob()
   {
-    Fixtures.loadModels("data.yml");
+    if (Member.count() == 0)
+    {
+      Fixtures.loadModels("data.yml");
+    }
   }
 }
